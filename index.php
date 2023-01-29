7a296c929b6223f14385e740e8f6673debfbf580
ivanbg1919 <irodic76@gmail.com>
2023-01-29 23:53:47 +0100

    sec  commit

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if (file_exists('todo.json')) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://bootswatch.com/5/cyborg/bootstrap.min.css" rel="stylesheet">
+    <link href="https://bootswatch.com/5/flatly/bootstrap.min.css" rel="stylesheet">
     <title>Todo App</title>
 </head>
 <body>
